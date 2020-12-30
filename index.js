0b3982d32ac349669a6d2291044ada3e242f90fc
NafiNgom <nafiangom@gmail.com>
2020-12-30 18:50:43 +0000

    monsecondsite

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 3000
+var meteo = require('./meteo.json')
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.get('/', (req, res) => {
+    res.render('page', { meteo: meteo })
+});
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+});
---
+++ public/script.js
@@ -0,0 +1,14 @@
+function ouvrirButton() {
+    if (confirm('voulait vous confirmer')) {
+        window.alert('vane ouver');
+    } else {
+        alert('action non validee');
+    }
+}
+
+function fermerButton() {
+    if (confirm('voulait vous confirmer')) {
+        window.alert('vane fermer');
+    }
+
+}
